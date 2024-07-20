635066e855e7d5f3cb9da6c66e747cd7fc490649
Mathieu GARNIER <mgarnier11@gmail.com>
2024-07-20 22:56:39 +0000

    feat: testing publish

---
+++ get-docker-versions/src/main.ts
@@ -0,0 +1,54 @@
+import core from '@actions/core';
+import github from '@actions/github';
+
+const getAllTags = async (url) => {
+  console.log(`Fetching ${url}`);
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  if (data.next) {
+    return [...data.results, ...(await getAllTags(data.next))];
+  } else {
+    return data.results;
+  }
+};
+
+const checkIfSemver = (tag) => {
+  // comes from : https://semver.org
+  const semverRegex =
+    /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/;
+
+  return semverRegex.test(tag);
+};
+
+const getVersions = async (url) => {
+  const tags = await getAllTags(url);
+
+  return tags.map((tag) => tag.name).filter((tag) => checkIfSemver(tag));
+};
+
+const main = async () => {
+  const apiUrl = process.env['INPUT_API-URL'];
+  const imageAuthor = process.env['INPUT_IMAGE-AUTHOR'];
+  const imageName = process.env['INPUT_IMAGE-NAME'];
+
+  const onlyLatest = process.env['INPUT_ONLY-LATEST'] === 'true';
+
+  const url = `${apiUrl}/${imageAuthor}/${imageName}/tags`;
+
+  const versions = await getVersions(url);
+
+  if (onlyLatest) {
+    console.log(`Only latest version: ${versions[0]}`);
+    core.setOutput('latest', versions[0]);
+  } else {
+    console.log(`All versions: ${versions}`);
+    core.setOutput('versions', versions);
+  }
+};
+
+main().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
